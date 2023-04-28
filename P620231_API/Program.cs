d4afc6b3a20266b5cc0f6f20d6e0b0d46cc29a27
Admin <Admin@DESKTOP-ULMSP3L>
2023-04-27 22:12:54 -0600

    Add project files.

---
+++ P620231_API/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using P620231_API.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+
+
+        var CnnStrBuilder = new SqlConnectionStringBuilder(
+            builder.Configuration.GetConnectionString("CNNSTR"));
+
+
+        string ccnStr = CnnStrBuilder.ConnectionString;
+
+        //definir la ccnstring al proyecto antes de iniciarlo 
+
+        builder.Services.AddDbContext<P620231_AutoAppoContext>(options => options.UseSqlServer(ccnStr)); 
+
+
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
